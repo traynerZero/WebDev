887a1fee2a19cdca1404dde792fb103665ef445c
Kenneth Tan <kennethtan_02@yahoo.com>
2018-01-11 01:00:18 +0800

    added login page

--- login.php
+++ login.php
@@ -5,13 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="main.css">
 	<!-- stylesheet -->
-	<!-- dependency: React.js -->
-    <script src="js/react-with-addons.js"></script>
-    <script src="js/react-dom.js"></script>
+
 
     <!--scripts-->
     <link href="css/literallycanvas.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/modals.css">
+    <link rel="stylesheet" type="text/css" href="css/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="js/literallycanvas.js"></script>
     <script src="js/jquery.js"></script>
@@ -21,8 +20,12 @@
 <body>
  <?php
     include('src/navbar.php');
-
  ?>
+<div class="login">
+    <input type="text" placeholder="Username" id="username">  
+  <input type="password" placeholder="password" id="password">  
+  <input type="submit" value="Sign In">
+</div>
 
 
 
