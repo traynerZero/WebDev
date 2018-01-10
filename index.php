f7fd2a4b8aa866b301fbb12558e3707a02da4203
Kenneth Tan <kennethtan_02@yahoo.com>
2018-01-11 00:46:13 +0800

    test branch commit

--- index.php
+++ index.php
@@ -18,28 +18,11 @@
     <script src="js/jquery-ui.js"></script>
     <script src="js/scripts.js"></script>
 </head>
-
-
-
-
 <body>
+ <?php
+    include('src/navbar.php');
 
-  <h2 style="font-family:cursive; font-size: 24px;" align="center"><a href="" style="color: black;">Paintstagram</a></h2>
-  <div class="navbar">
-    <div class="dropdown">
-    <button class="dropbtn" style="margin-right: 40px;">
-      <span class="fa fa-bars"></span>
-    </button>
-    <div class="dropdown-content" style="right: 0; left: auto;">
-      <a href="#">Account Settings</a>
-      <a href="#">Logout</a>
-    </div>
-  </div>
-  <a href="#" title="Message"><span class=" fa fa-envelope"></span></a>
-  <a href="#" title="Notification"><span class="fa fa-bell"></span></a>
-  <a href="#home" title="Home"><span class="fa fa-home"></span></a>
-  <a href="#"><img src="/moreimg/img_avatar.png" class="profile-img" style="height: 25px; width: 25px;"></a>
-</div>
+ ?>
 
             <button type="button" style="width: 100%; padding: 10px; " class="letspaint"><strong>LET'S PAINT</strong></button>
 <div class="panel">
