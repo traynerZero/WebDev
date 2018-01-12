2b64542564f4a098d21070da97b871f911a30d31
Kenneth Tan <kennethtan_02@yahoo.com>
2018-01-13 01:33:40 +0800

    redid profile design

--- header.php
+++ header.php
@@ -1,18 +1,20 @@
-<h2 style="font-family:cursive; font-size: 24px;" align="center"><a href="index.php" style="color: black;">Paintstagram</a></h2>
-  <div class="navbar">
-    <div class="dropdown">
-    <button class="dropbtn" style="margin-right: 40px;">
-      <span class="fa fa-bars"></span>
-    </button>
-    <div class="dropdown-content" style="right: 0; left: auto;">
-      <a href="editaccount.php">Account Settings</a>
-      <a href="about.php">About</a>
-      <a href="">Privacy Policy</a>
-      <a href="login.php">Logout</a>
+<div class = "app-header">
+  <h2 style="font-family:cursive; font-size: 24px;" align="center"><a href="index.php" style="color: black;">Paintstagram</a></h2>
+    <div class="navbar">
+      <div class="dropdown">
+      <button class="dropbtn" style="margin-right: 40px;">
+        <span class="fa fa-bars"></span>
+      </button>
+      <div class="dropdown-content" style="right: 0; left: auto;">
+        <a href="editaccount.php">Account Settings</a>
+        <a href="about.php">About</a>
+        <a href="">Privacy Policy</a>
+        <a href="login.php">Logout</a>
+      </div>
     </div>
+    <a href="#" title="Message" class="dropbtn"><span class=" fa fa-envelope"></span></a>
+    <a href="#" title="Notification"><span class="fa fa-bell"></span></a>
+    <a href="home.php" title="Home"><span class="fa fa-home"></span></a>
+    <a href="profile.php"><img src="/moreimg/img_avatar.png" class="profile-img" style="height: 25px; width: 25px;"></a>
   </div>
-  <a href="#" title="Message" class="dropbtn"><span class=" fa fa-envelope"></span></a>
-  <a href="#" title="Notification"><span class="fa fa-bell"></span></a>
-  <a href="home.php" title="Home"><span class="fa fa-home"></span></a>
-  <a href="#"><img src="/moreimg/img_avatar.png" class="profile-img" style="height: 25px; width: 25px;"></a>
 </div>
--- profile.php
+++ profile.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Paintstagram</title>
+    <title>Paintstagram</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="main.css">
-	<!-- stylesheet -->
-	<!-- dependency: React.js -->
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <!-- stylesheet -->
+    <!-- dependency: React.js -->
     <script src="js/react-with-addons.js"></script>
     <script src="js/react-dom.js"></script>
 
@@ -20,33 +21,39 @@
     <script src="js/profile.js"></script>
     <script src="js/scripts.js"></script>
 </head>
+
 <body>
- <?php
+    <?php
     include('src/navbar.php');
-
  ?>
-<h1>GitSocial <small>A social network for programmers.</small></h1>
-<header>
-  <figure class="profile-banner">
-    <img src="https://unsplash.it/975/300" alt="Profile banner" />
-  </figure>
-  <figure class="profile-picture" 
-    style="background-image: url('http://unsplash.it/150/150')">
-  </figure>
-  <div class="profile-stats">
-    <ul>
-      <li>13    <span>Projects</span></li>
-      <li>1,354 <span>Commits</span></li>
-      <li>32    <span>Following</span></li>
-      <li>324   <span>Followers</span></li>
-    </ul>
-    <a href="javascript:void(0);" class="follow">
-      Follow Nick
-    </a>
-  </div>
-  <h1>Nick Dobie <small>nrdobie</small></h1>
-</header>
 
+        <div id="user_header">
+            <div id="user">
+
+                <div id="user_picture">
+                    <div id="picture_box">
+                        <img src="https://en.gravatar.com/userimage/54162376/6fa5c4908077ceddb6388f7cad1a1187.jpg?size=100">
+                    </div>
+                </div>
+                <div id="user_info">
+                    <h2 class="user_name">Robbie Green</h2>
+                    <hr>
+                    <ul id="user_list">
+                        <li>
+                            <i class="icon-building info_icon"></i>
+                            </span> Advantage Screen Printing</li>
+                        <li>
+                            <span class="info_icon">
+                                <i class="icon-briefcase"></i>
+                            </span> Creative Director</li>
+                        <li>
+                            <span class="info_icon">
+                                <i class="icon-book"></i>
+                            </span> BFA In Graphic Arts</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
 
 </body>
 
